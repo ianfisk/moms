cccd093061bf243f7a806a25b59e2c4f00ac2f05
Ian Fisk <i.fisk32@gmail.com>
2018-05-12 23:06:15 -0700

    Add service worker.

---
+++ sw.js
@@ -0,0 +1,30 @@
+const cacheName = 'moms-v1';
+
+self.addEventListener('install', event => {
+	event.waitUntil(
+		caches.open(cacheName).then(cache => {
+			return cache.addAll([
+				'./',
+				'index.html',
+				'message.html',
+				'css/common.css',
+				'css/index.css',
+				'css/message.css',
+				'fonts/Montserrat-Regular.ttf',
+				'images/bg.png',
+				'images/down-arrow.png',
+				'js/index.js',
+				'js/message.js',
+				'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
+				'https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.4/TweenLite.min.js',
+				'https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.4/plugins/ScrollToPlugin.min.js',
+			]);
+		})
+	);
+});
+
+self.addEventListener('fetch', event => {
+	event.respondWith(
+		caches.match(event.request).then(response => response || fetch(event.request))
+	);
+});
